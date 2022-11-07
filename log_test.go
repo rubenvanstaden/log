aa017746b6f6444ccd2efc99291c35e76aa4588e
Ruben van Staden <ruben@anyonsys.com>
2022-11-07 15:53:03 -0500

    test: add error, warn, info tests

--- log_test.go
+++ log_test.go
@@ -45,98 +45,74 @@ func TestUnit_Debug(t *testing.T) {
 	}
 }
 
-func TestUnitInfo(t *testing.T) {
+func TestUnit_Info(t *testing.T) {
 
-	// tests := []tester{
-	// 	{
-	// 		message: "hello, world!",
-	// 		pattern: fmt.Sprintf("test: pid=%s %s %s DEBUG: hello, world!\n$", rgxPID, rgxdate, rgxtime),
-	// 	},
-	// }
-	//
-	// for _, tc := range tests {
-	//
-	// 	var buf bytes.Buffer
-	//
-	// 	logger := log.NewLogger("test", &buf)
-	// 	logger.Debug(tc.message)
-	//
-	// 	act := buf.String()
-	//
-	// 	matched, err := regexp.MatchString(tc.pattern, act)
-	// 	test.Ok(t, err)
-	// 	test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
-	//     }
+	tests := []tester{
+		{
+			message: "hello, world!",
+			pattern: fmt.Sprintf("test: pid=%s %s %s INFO: hello, world!\n$", rgxPID, rgxdate, rgxtime),
+		},
+	}
+	
+	for _, tc := range tests {
+	
+		var buf bytes.Buffer
+	
+		logger := log.NewLogger("test", &buf)
+		logger.Info(tc.message)
+	
+		act := buf.String()
+	
+		matched, err := regexp.MatchString(tc.pattern, act)
+		test.Ok(t, err)
+		test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
+    }
 }
 
-func TestUnitWarn(t *testing.T) {
+func TestUnit_Warn(t *testing.T) {
 
-	// tests := []tester{
-	// 	{
-	// 		message: "hello, world!",
-	// 		pattern: fmt.Sprintf("test: pid=%s %s %s DEBUG: hello, world!\n$", rgxPID, rgxdate, rgxtime),
-	// 	},
-	// }
-	//
-	// for _, tc := range tests {
-	//
-	// 	var buf bytes.Buffer
-	//
-	// 	logger := log.NewLogger("test", &buf)
-	// 	logger.Debug(tc.message)
-	//
-	// 	act := buf.String()
-	//
-	// 	matched, err := regexp.MatchString(tc.pattern, act)
-	// 	test.Ok(t, err)
-	// 	test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
-	//     }
+	tests := []tester{
+		{
+			message: "hello, world!",
+			pattern: fmt.Sprintf("test: pid=%s %s %s WARN: hello, world!\n$", rgxPID, rgxdate, rgxtime),
+		},
+	}
+	
+	for _, tc := range tests {
+	
+		var buf bytes.Buffer
+	
+		logger := log.NewLogger("test", &buf)
+		logger.Warn(tc.message)
+	
+		act := buf.String()
+	
+		matched, err := regexp.MatchString(tc.pattern, act)
+		test.Ok(t, err)
+		test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
+    }
 }
 
-func TestUnitError(t *testing.T) {
+func TestUnit_Error(t *testing.T) {
 
-	// tests := []tester{
-	// 	{
-	// 		message: "hello, world!",
-	// 		pattern: fmt.Sprintf("test: pid=%s %s %s DEBUG: hello, world!\n$", rgxPID, rgxdate, rgxtime),
-	// 	},
-	// }
-	//
-	// for _, tc := range tests {
-	//
-	// 	var buf bytes.Buffer
-	//
-	// 	logger := log.NewLogger("test", &buf)
-	// 	logger.Debug(tc.message)
-	//
-	// 	act := buf.String()
-	//
-	// 	matched, err := regexp.MatchString(tc.pattern, act)
-	// 	test.Ok(t, err)
-	// 	test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
-	//     }
-}
-
-func TestUnitFatal(t *testing.T) {
-
-	// tests := []tester{
-	// 	{
-	// 		message: "hello, world!",
-	// 		pattern: fmt.Sprintf("test: pid=%s %s %s DEBUG: hello, world!\n$", rgxPID, rgxdate, rgxtime),
-	// 	},
-	// }
-	//
-	// for _, tc := range tests {
-	//
-	// 	var buf bytes.Buffer
-	//
-	// 	logger := log.NewLogger("test", &buf)
-	// 	logger.Debug(tc.message)
-	//
-	// 	act := buf.String()
-	//
-	// 	matched, err := regexp.MatchString(tc.pattern, act)
-	// 	test.Ok(t, err)
-	// 	test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
-	//     }
+	tests := []tester{
+		{
+			message: "hello, world!",
+			pattern: fmt.Sprintf("test: pid=%s %s %s ERROR: hello, world!\n$", rgxPID, rgxdate, rgxtime),
+		},
+	}
+	
+	for _, tc := range tests {
+	
+		var buf bytes.Buffer
+	
+		logger := log.NewLogger("test", &buf)
+		logger.Error(tc.message)
+	
+		act := buf.String()
+	
+		matched, err := regexp.MatchString(tc.pattern, act)
+		test.Ok(t, err)
+		test.Assert(t, matched, fmt.Sprintf("pattern match failed: %q", act))
+    }
 }
